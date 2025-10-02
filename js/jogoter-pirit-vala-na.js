7ee0720f8ff39dcde9062cf0b5928a93ca90ec11
masumbillah6778 <102429423+masumbillah6778@users.noreply.github.com>
2025-10-02 20:41:47 +0600

    Update jogoter-pirit-vala-na.js

--- js/jogoter-pirit-vala-na.js
+++ js/jogoter-pirit-vala-na.js
@@ -4,7 +4,7 @@ var _data = '';
 var currentLine = '';
 mounted();
 async function setLyric() {
-  await fetch('https://masumbillah6778.github.io/music-lyrics/js/lyrics/jogoter-pirit-vala-na.js', {
+  await fetch('https://masumbillah6778.github.io/music-lyrics/js/lyrics/jogoter-pirit-vala-na.json', {
     method: 'GET',
   })
     .then((response) => response.json())
