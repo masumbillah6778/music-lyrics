dea1913d600f75795fdee747d128cd46f9924aa6
masumbillah6778 <102429423+masumbillah6778@users.noreply.github.com>
2025-11-25 00:01:57 +0600

    Update amar-mon-pakhi.js

--- js/amar-mon-pakhi.js
+++ js/amar-mon-pakhi.js
@@ -4,7 +4,7 @@ var _data = '';
 var currentLine = '';
 mounted();
 async function setLyric() {
-  await fetch('js/lyrics/amar-mon-pakhi.json', {
+  await fetch('https://masumbillah6778.github.io/music-lyrics/js/lyrics/tor-pirite-pagol-hoilam-re.json', {
     method: 'GET',
   })
     .then((response) => response.json())
